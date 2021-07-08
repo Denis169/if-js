3324ca7ae318284467ca9eda636b2e59fd5afe0c
Denis169 <denis6125038@gmail.com>
2021-07-08 21:53:28 +0300

    lesson-2-1 did everything according to those assignments. I played around with primitive commands and practiced outputting to the console.

---
+++ main.js
@@ -0,0 +1,28 @@
+// 6th item of homework
+
+var user = 'John Doe';
+console.log(user);
+
+var student = 'Denis logimakhov';
+console.log(student);
+
+student = user;
+// In the user variable now 'John Doe'
+console.log(user);
+
+// 7th item of homework:
+
+var test = 1;
+test++;
+test = test + '1';
+// In the test variable now 21
+console.log(test);
+test = test - 1;
+// In the test variable now 20
+console.log(test);
+test = !!test;
+// In the test variable now tru
+console.log(test);
+
+
+
