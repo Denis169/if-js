fd400243e09255c46c4e85d9ba2407de5d8dec74
Denis169 <denis6125038@gmail.com>
2021-07-12 23:14:36 +0300

    Fixed bugs according to commits

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
 // 1-st task
-let array = [2,3,5,8];
+const array = [2, 3, 5, 8];
 let composition = 1;
-for (let i=0; i < array.length; i++){
+for (let i = 0; i < array.length; i++){
   composition *= array[i];
 }
 console.log(`Composition of array ${array} = ${composition}`);
 
 // 2-nd task
-let value = [];
-let massiv = [2,5,8,15,0,6,20,3];
+const massiv = [2, 5, 8, 15, 0, 6, 20, 3];
 console.log(`Element greater than 5 but less than 10 from ${massiv} is:`);
-for (let i=0; i < massiv.length; i++){
-  if (massiv[i] > 5){
-    if (massiv[i] < 10){
-      value.push(massiv[i]);
-    }
+for (let i = 0; i < massiv.length; i++){
+  if (massiv[i] > 5 && massiv[i] < 10){
+    console.log(massiv[i]);
   }
 }
-console.log(`   ${value}`);
 
 // 3-rd task
-let even = [];
 console.log(`Even numbers of array ${massiv} is:`)
-for (let i=0; i < massiv.length; i++){
-  if (massiv[i]%2 == 0){
-    even.push(massiv[i]);
+for (let i = 0; i < massiv.length; i++){
+  if (massiv[i] % 2 == 0){
+    console.log(massiv[i]);
   }
 }
-console.log(`   ${even}`);
