13e5ad13fcf0fea97ccc9983e496eb9dab7532a7
Denis169 <denis6125038@gmail.com>
2021-07-12 20:22:07 +0300

    change var on let

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 // 6th item of homework
 
-var user = 'John Doe';
+let user = 'John Doe';
 console.log(user);
 
-var student = 'Denis logimakhov';
+let student = 'Denis logimakhov';
 console.log(student);
 
 student = user;
+
 // In the user variable now 'John Doe'
+
 console.log(user);
 
 // 7th item of homework:
 
-var test = 1;
+let test = 1;
 test++;
 test = test + '1';
+
 // In the test variable now 21
+
 console.log(test);
 test = test - 1;
+
 // In the test variable now 20
+
 console.log(test);
 test = !!test;
+
 // In the test variable now tru
+
 console.log(test);
 
 
