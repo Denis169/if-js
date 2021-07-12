bb9fc8c7a9c8ede338ef08d74c65d97a6f0aa4ac
Denis__Logimakhov <84120857+Denis169@users.noreply.github.com>
2021-07-12 22:02:58 +0300

    lesson-2-1 did everything according to those assignments. I played ar… (#2)
    * lesson-2-1 did everything according to those assignments. I played around with primitive commands and practiced outputting to the console.
    
    * change var on let

---
+++ main.js
@@ -0,0 +1,36 @@
+// 6th item of homework
+
+let user = 'John Doe';
+console.log(user);
+
+let student = 'Denis logimakhov';
+console.log(student);
+
+student = user;
+
+// In the user variable now 'John Doe'
+
+console.log(user);
+
+// 7th item of homework:
+
+let test = 1;
+test++;
+test = test + '1';
+
+// In the test variable now 21
+
+console.log(test);
+test = test - 1;
+
+// In the test variable now 20
+
+console.log(test);
+test = !!test;
+
+// In the test variable now tru
+
+console.log(test);
+
+
+
