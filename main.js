71c9f628227690600912e420d6bd073a709c19fa
Denis169 <denis6125038@gmail.com>
2021-07-19 23:33:57 +0300

    Fixed bugs according to comments

--- main.js
+++ main.js
@@ -1,39 +1,7 @@
-// // 1-st task
-// const array = [2, 3, 5, 8];
-// let composition = 1;
-// for (let i = 0; i < array.length; i++){
-//   composition *= array[i];
-// }
-// console.log(`Composition of array ${array} = ${composition}`);
-
-// // 2-nd task
-// const massiv = [2, 5, 8, 15, 0, 6, 20, 3];
-// console.log(`Element greater than 5 but less than 10 from ${massiv} is:`);
-// for (let i = 0; i < massiv.length; i++){
-//   if (massiv[i] > 5 && massiv[i] < 10){
-//     console.log(massiv[i]);
-//   }
-// }
-
-// // 3-rd task
-// console.log(`Even numbers of array ${massiv} is:`)
-// for (let i = 0; i < massiv.length; i++){
-//   if (massiv[i] % 2 === 0){
-//     console.log(massiv[i]);
-//   }
-// }
-
-// 3-rd LESSON
 // Palindrom
-function palindrom(word) {
-  const array = word.split('');
-  array.reverse();
-  const newWord = array.join('');
-  return (word === newWord);
-}
-
+const palindrom = (word) => word === word.split('').reverse().join('');
+  
 console.log(palindrom('atata'));
-
 
 // Функция min(a, b) и функция max(a,b)
 
@@ -47,8 +15,8 @@ console.log(max(12,6));
 
 // Замена элементов массива
 
-const numbers =  [2, 4, 5, 8, 0, 8, 8, 0, 78, 95];
-const nul = numbers.map(function (num) { 
-  return num === 0 ? 'zero': num
-});
+const numbers =  [2, 40, 502, 8, 0, 8, 8, 0, 78, 95];
+
+const nul = numbers.toString().replace(/0/g, 'zero').split(',').map (toZero = (num) => !!(Number(num)) ? Number(num): num );
+
 console.log(nul);
