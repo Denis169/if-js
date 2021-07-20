e7c6c9c94aa5f8ae8a6c6b074a787038089eed40
Denis169 <denis6125038@gmail.com>
2021-07-20 10:41:38 +0300

    change function

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ console.log(max(12,6));
 
 // Замена элементов массива
 
-const nul = (numbers) => numbers.map (toZero = (num) => num.toString().includes(0) ? num.toString().replace(/0/g, 'zero'): num );
+const change = (numbers) => numbers.map (toZero = (num) => num.toString().includes(0) ? num.toString().replace(/0/g, 'zero'): num );
 
 const numbers =  [2, 40, 502, 8, 0, 8, 8, 0, 78, 95];
-console.log(nul(numbers));
+console.log(change(numbers));
