bb8bafe9b381cb0ed9a9e68ce077f709c9b9aea1
Denis169 <denis6125038@gmail.com>
2021-07-26 21:06:33 +0300

    start projekt

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ let counterTextOne = сounter();
 let counterTextTwo = сounter();
 let counterTextThree = сounter();
 
+let a = b;
 
 const elemTextFirst = document.getElementById('text_1');
 elemTextFirst.addEventListener('click', function(e) {e.target.style.color = colorsForText[counterTextOne()]});
