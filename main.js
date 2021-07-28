576ca63cd4f7e8ac48a24537b589abca20cce6bb
Denis169 <denis6125038@gmail.com>
2021-07-28 07:47:51 +0300

    husky and test install

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 // Возвращение суммы
-const sum = (a) => (b) => a+b;
+const sum = (a) => (b) => a + b;
 
-console.log(sum(5)(2));
+console.log(sum(2)(3));
 
 // Покраска текста <p>
 
 const colorsForText = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
-function сounter() {
-  let currentCount = 0;
-  return () => currentCount === 5 ? currentCount = 0 : currentCount++;
-}
-
-
-let counterTextOne = сounter();
-let counterTextTwo = сounter();
-let counterTextThree = сounter();
-
-
+const changeColor = () => {
+  let count = 0;
+  return (e) => {
+    e.target.style.color = colorsForText[count];
+    count === 5 ? count = 0 : count++;
+  };
+};
 
 const elemTextFirst = document.getElementById('text_1');
-elemTextFirst.addEventListener('click', function(e) {e.target.style.color = colorsForText[counterTextOne()]});
+elemTextFirst.addEventListener('click', changeColor());
 
 const elemTextTwo = document.getElementById('text_2');
-elemTextTwo.addEventListener('click', function(e) {e.target.style.color = colorsForText[counterTextTwo()]});
+elemTextTwo.addEventListener('click', changeColor());
 
 const elemTextThree = document.getElementById('text_3');
-elemTextThree.addEventListener('click', function(e) {e.target.style.color = colorsForText[counterTextThree()]});
+elemTextThree.addEventListener('click', changeColor());
+
 
