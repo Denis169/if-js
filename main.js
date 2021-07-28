c27bee88a815a0fb4c17094715317bea2db41a14
Denis169 <denis6125038@gmail.com>
2021-07-28 21:59:42 +0300

    Lesson-4: change e to event, groupped const and listener

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Возвращение суммы
-const sum = (a) => (b) => a+b;
+const sum = (a) => (b) => a + b;
 
 console.log(sum(5)(2));
 
@@ -9,19 +9,18 @@ const colorsForText = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue']
 
 const changeColor = () => {
   let count = 0;
-  return (e) => {
-    e.target.style.color = colorsForText[count];
+  return (event) => {
+    event.target.style.color = colorsForText[count];
     count === 5 ? count = 0 : count++;
   };
 };
 
 const elemTextFirst = document.getElementById('text_1');
-elemTextFirst.addEventListener('click', changeColor());
-
 const elemTextTwo = document.getElementById('text_2');
-elemTextTwo.addEventListener('click', changeColor());
-
 const elemTextThree = document.getElementById('text_3');
+
+elemTextFirst.addEventListener('click', changeColor());
+elemTextTwo.addEventListener('click', changeColor());
 elemTextThree.addEventListener('click', changeColor());
 
 
