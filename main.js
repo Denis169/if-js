25e490ee7979a76b980a7e6b5908ad1fd91d1701
Denis__Logimakhov <84120857+Denis169@users.noreply.github.com>
2021-08-19 11:00:45 +0300

    Lesson 8 (#9)
    * Lesson-8: great new branch
    
    * Lesson-8: Deep Equal
    
    * lesson-8: Calendar with object
    
    * lesson-8: Calendar with object
    
    * lesson-8: change var on let
    
    * lesson-8: change 'for in' on 'for of'
    
    * lesson-8: fix bugs according to commits
    
    * lesson-8: fixed ternarnyi operator
    
    * lesson-8:added check for array. Optimized the code.

--- main.js
+++ main.js
@@ -1,229 +1,65 @@
-// Palindrom
+// Deep Equal
 
-const palindrom = (word) => word === word.split('').reverse().join('');
-  
-console.log(palindrom('atata'));
+const obj1 = {
+  a: 'a',
+  b: {
+    a: 'a',
+    b: 'b',
+    c: {
+      a: 1,
+    },
+  },
+};
+const obj2 = {
+  b: {
+    c: {
+      a: 1,
+    },
+    b: 'b',
+    a: 'a',
+  },
+  a: 'a',
+};
+const obj3 = {
+  a: {
+    c: {
+      a: 'a',
+    },
+    b: 'b',
+    a: 'a',
+  },
+  b: 'b',
+};
 
-// Search of object placement
-const data = [
-  {
-    country: 'Slowakia',
-    city: 'Vysokie Tatry',
-    hotel: 'Villa Kunerad',
-  },
-  {
-    country: 'Germany',
-    city: 'Berlin',
-    hotel: 'Hostel Friendship',
-  },
-  {
-    country: 'Indonesia',
-    city: 'Bali',
-    hotel: 'Ubud Bali Resort&SPA',
-  },
-  {
-    country: 'Netherlands',
-    city: 'Rotterdam',
-    hotel: 'King Kong Hostel',
-  },
-  {
-    country: 'Marocco',
-    city: 'Ourika',
-    hotel: 'Rokoko Hotel',
-  },
-  {
-    country: 'Germany',
-    city: 'Berlin',
-    hotel: 'Hotel Rehberge Berlin Mitte',
-  },
-];
+const deepEqual = (object1, object2 ) => {
+  if  (Object.keys(object1).length !== Object.keys(object2).length) return false;
+  for(let key of Object.keys(object1)) {
+    if (!Object.keys(object2).includes(key)) return false;
+    if (Array.isArray(object1[`${key}`]) || Array.isArray(object2[`${key}`])) return 'The passed objects have an array. Execution is impossible.';
+    if(object1[key] !== object2[key] && !deepEqual(object1[key], object2[key])) return false;
+  }
+  return true;
+};
 
-const filterPrices = (array, value) => {
-  value = new RegExp(value,'igm');
-  return array.reduce((acc, obj) => value.test(Object.values(obj)) ? acc + `${Object.values(obj).join(', ')}\n` : acc, []);
-}
-  
-console.log(filterPrices(data, 'Germany'));
+console.log(deepEqual(obj1, obj2));
+console.log(deepEqual(obj1, obj3));
 
-// Mapping Countries to Array Cities
-const hotels = [
-  {
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-  },
-  {
-    name: 'Apartment Sunshine',
-    city: 'Santa Cruz de Tenerife',
-    country: 'Spain',
-  },
-  {
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-  },
-  {
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-  },
-  {
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-  },
-  {
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-  },
-  {
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Virgin Hotel',
-    city: 'Chicago',
-    country: 'USA',
-  },
-  {
-    name: 'Grand Beach Resort',
-    city: 'Dubai',
-    country: 'United Arab Emirates',
-  },
-  {
-    name: 'Shilla Stay',
-    city: 'Seoul',
-    country: 'South Korea',
-  },
-  {
-    name: 'San Firenze Suites',
-    city: 'Florence',
-    country: 'Italy',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Black Penny Villas',
-    city: 'BTDC, Nuca Dua',
-    country: 'Indonesia',
-  },
-  {
-    name: 'Koko Hotel',
-    city: 'Tokyo',
-    country: 'Japan',
-  },
-  {
-    name: 'Ramada Plaza',
-    city: 'Istanbul',
-    country: 'Turkey',
-  },
-  {
-    name: 'Waikiki Resort Hotel',
-    city: 'Hawaii',
-    country: 'USA',
-  },
-  {
-    name: 'Puro Hotel',
-    city: 'Krakow',
-    country: 'Poland',
-  },
-  {
-    name: 'Asma Suites',
-    city: 'Santorini',
-    country: 'Greece',
-  },
-  {
-    name: 'Cityden Apartments',
-    city: 'Amsterdam',
-    country: 'Netherlands',
-  },
-  {
-    name: 'Mandarin Oriental',
-    city: 'Miami',
-    country: 'USA',
-  },
-  {
-    name: 'Concept Terrace Hotel',
-    city: 'Rome',
-    country: 'Italy',
-  },
-  {
-    name: 'Ponta Mar Hotel',
-    city: 'Fortaleza',
-    country: 'Brazil',
-  },
-  {
-    name: 'Four Seasons Hotel',
-    city: 'Sydney',
-    country: 'Australia',
-  },
-  {
-    name: 'Le Meridien',
-    city: 'Nice',
-    country: 'France',
-  },
-  {
-    name: 'Apart Neptun',
-    city: 'Gdansk',
-    country: 'Poland',
-  },
-  {
-    name: 'Lux Isla',
-    city: 'Ibiza',
-    country: 'Spain',
-  },
-  {
-    name: 'Nox Hostel',
-    city: 'London',
-    country: 'UK',
-  },
-  {
-    name: 'Leonardo Vienna',
-    city: 'Vienna',
-    country: 'Austria',
-  },
-  {
-    name: 'Adagio Aparthotel',
-    city: 'Edinburgh',
-    country: 'UK',
-  },
-  {
-    name: 'Steigenberger Hotel',
-    city: 'Hamburg',
-    country: 'Germany',
-  },
-];
-
-const countryCity = (array) => {
-  return array.reduce((finalCountryCity, obj) => {
-    Object.keys(finalCountryCity).includes(obj.country) 
-      ? finalCountryCity[`${obj.country}`].push(obj.city)
-      : finalCountryCity[`${obj.country}`] = [obj.city];
-    return finalCountryCity;
-  }, {});
-}
-
-console.log(countryCity(hotels));
 
 //Calendar
+const dateUser = {
+  checkInDate: 28,
+  checkInDateInMonth: false,
+  checkOutDate: 10,
+  checkOutDateInMonth: true,
+};
 
-
-const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
+const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek, daysUser) => {
   let amountDayInMonth = (daysInMonth + (dayOfWeek - 1)) % daysInWeek === 0 //Checking to see if there’s a moving week at the end
     ? daysInMonth + (dayOfWeek - 1) // if not a week at the end
     : (daysInWeek - (daysInMonth + (dayOfWeek - 1)) % daysInWeek + daysInMonth + (dayOfWeek - 1)); //If there is a week 
   let month = [];
   let weekOfCalendar = [];
+  let dataDay = {};
   
   if (dayOfWeek > daysInWeek) { 
     return console.log('Err: the wrong dayOfWeek')
@@ -231,12 +67,41 @@ const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
 
   for (let day = 1; day <= amountDayInMonth; day++) {
     if (day <= dayOfWeek - 1) {
-      weekOfCalendar.push(daysInMonth - (dayOfWeek-1) + day);
+      dataDay.dayOfMonth = daysInMonth - (dayOfWeek-1) + day;
+      dataDay.notCurrentMonth = true;
+      if (daysUser.checkInDateInMonth) {
+        dataDay.selectedDay = false;
+      } else {
+        daysUser.checkInDate <= dataDay.dayOfMonth ? dataDay.selectedDay = true : dataDay.selectedDay = false;
+      }
+    weekOfCalendar.push(dataDay);
+    dataDay = {};
+
     } else if (day <= daysInMonth + (dayOfWeek-1)) {
-      weekOfCalendar.push(day - (dayOfWeek - 1));
+      dataDay.dayOfMonth = day - (dayOfWeek - 1);
+      dataDay.notCurrentMonth = false;
+      if (!daysUser.checkInDateInMonth && !daysUser.checkOutDateInMonth){
+        dataDay.selectedDay = true;
+      } else if (daysUser.checkOutDateInMonth) {
+        dataDay.dayOfMonth <= daysUser.checkOutDate ? dataDay.selectedDay = true : dataDay.selectedDay = false;
+      } else {
+        dataDay.dayOfMonth >= daysUser.checkInDate ? dataDay.selectedDay = true : dataDay.selectedDay = false;
+      }
+      weekOfCalendar.push(dataDay);
+      dataDay = {};
+    
     } else {
-      weekOfCalendar.push(day - (dayOfWeek - 1) - daysInMonth);
+      dataDay.dayOfMonth = day - (dayOfWeek - 1) - daysInMonth;
+      dataDay.notCurrentMonth = true;
+      if (daysUser.checkOutDateInMonth) {
+        dataDay.selectedDay = false;
+      } else {
+        dataDay.dayOfMonth <= daysUser.checkOutDate ? dataDay.selectedDay = true : dataDay.selectedDay = false;
+      }
+      weekOfCalendar.push(dataDay);
+      dataDay = {};
     }
+
     if (weekOfCalendar.length === daysInWeek) {
       month.push(weekOfCalendar);
       weekOfCalendar = [];
@@ -245,4 +110,4 @@ const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
   return month;
 }
 
-console.log(getCalendarMonth(10, 10, 10));
+console.log(getCalendarMonth(30, 7, 7, dateUser));
