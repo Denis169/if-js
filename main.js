a89c9f3c9b19d43b473329f2e91eb9b2fe75d934
Denis169 <denis6125038@gmail.com>
2021-08-20 15:59:15 +0300

    lesson-10: made a calendar with current dates

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-// // Coloring text <p> 2 "First blood 2"
+// Coloring text <p> 2 "First blood 2"
 
 
 
-// const elemTextFirst = document.getElementById('text_1');
-// const elemTextTwo = document.getElementById('text_2');
-// const elemTextThree = document.getElementById('text_3');
+const elemTextFirst = document.getElementById('text_1');
+const elemTextTwo = document.getElementById('text_2');
+const elemTextThree = document.getElementById('text_3');
 
-// const changeColor = () => {
-//   const colors = {
-//     data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
-//     [Symbol.iterator]() {
-//       return this;
-//     },
-//     next() {
-//       this.current++
-//       if (this.current < this.data.length) {
-//         return {
-//           done: false,
-//           value: this.data[this.current],
-//         };
-//       } else {
-//         this.current = 0;
-//         return {
-//           done: true,
-//           value: this.data[this.current],
-//         };
-//       }
-//     }
-//   };
-//   return (event) => event.target.style.color = colors.next(colors).value;
-// };
+const changeColor = () => {
+  const colors = {
+    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      this.current++
+      if (this.current < this.data.length) {
+        return {
+          done: false,
+          value: this.data[this.current],
+        };
+      } else {
+        this.current = 0;
+        return {
+          done: true,
+          value: this.data[this.current],
+        };
+      }
+    }
+  };
+  return (event) => event.target.style.color = colors.next(colors).value;
+};
 
-// elemTextFirst.addEventListener('click', changeColor());
-// elemTextTwo.addEventListener('click', changeColor());
-// elemTextThree.addEventListener('click', changeColor());
+elemTextFirst.addEventListener('click', changeColor());
+elemTextTwo.addEventListener('click', changeColor());
+elemTextThree.addEventListener('click', changeColor());
 
 
 //Calendar
