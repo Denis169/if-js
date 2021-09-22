1f80482b986b940491dd56af55758f86b152facc
Denis Logimahov <denis6125038@gmail.com>
2021-09-22 19:33:13 +0300

    lesson-15: fixed mistakes

--- 1/main.js
+++ 1/main.js
@@ -1,13 +1,16 @@
 const formSendFile = document.getElementById('form');
 
-formSendFile.addEventListener('submit', event => {
+const sendFile = (event) => {
   event.preventDefault();
   const formData = new FormData(formSendFile);
-  const reader = new FileReader();
-
+  
   fetch('https://fe-student-api.herokuapp.com/api/file', {
     method: 'POST',
     body: formData,
   })  .then(response => response.json())
-      .then(data => console.log(data));
-});
+      .then(data => console.log(data))
+      .catch(err => console.log('Fetch err:', err));
+};
+
+
+formSendFile.addEventListener('submit', sendFile)
